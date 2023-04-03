89c73858e5083de5f7ee2685febe25e575d2eb19
Sejal Makwana <makwana.sejal1994@gmail.com>
2023-04-03 18:43:25 +0530

    New commit message

---
+++ src/main/java/dynamicwebproject/logincals.java
@@ -0,0 +1,10 @@
+package dynamicwebproject;
+
+public class logincals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("dynamic web project");
+	}
+
+}

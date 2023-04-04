b35d9b15ac664d46979093ec242632bcd3210c9c
sejal makwana <makwana.sejal1994@gmail.com>
2023-04-04 15:10:06 +0530

    commiting sejal java project

--- src/main/java/dynamicwebproject/SEJALCAL.java
+++ src/main/java/dynamicwebproject/SEJALCAL.java
@@ -1,10 +1,10 @@
 package dynamicwebproject;
 
-public class logincals {
+public class SEJALCAL {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("dynamic web project for git changes");
+		System.out.println("SEJAL PROJECT");
 	}
 
 }

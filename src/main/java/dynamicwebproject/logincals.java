20d0a411bbdfe3b390d9ec10565dadf76c698e70
sejal makwana <makwana.sejal1994@gmail.com>
2023-04-04 16:47:14 +0530

    changes in develop branch

--- src/main/java/dynamicwebproject/logincals.java
+++ src/main/java/dynamicwebproject/logincals.java
@@ -4,7 +4,7 @@ public class logincals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("dynamic web project with git version control system");
+ System.out.println("dynamic web project with git new branch ");
 	}
 
 }

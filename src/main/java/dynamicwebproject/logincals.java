88ea48dac8a50a1d5ddf4ba32948283cd8f47a80
sejal makwana <makwana.sejal1994@gmail.com>
2023-04-04 15:50:34 +0530

    adding new content

--- src/main/java/dynamicwebproject/logincals.java
+++ src/main/java/dynamicwebproject/logincals.java
@@ -4,7 +4,7 @@ public class logincals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("dynamic web project for git changes");
+ System.out.println("dynamic web project with git version control system");
 	}
 
 }

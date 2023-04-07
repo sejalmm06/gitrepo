ef81e88732d28932e6375f2813f5182788d2827d
sejal makwana <makwana.sejal1994@gmail.com>
2023-04-07 10:52:10 +0530

    added develop new word

--- src/main/java/dynamicwebproject/logincals.java
+++ src/main/java/dynamicwebproject/logincals.java
@@ -4,7 +4,7 @@ public class logincals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("dynamic web project with git new branch ");
+ System.out.println("dynamic web project with git new branch develop ");
 	}
 
 }
